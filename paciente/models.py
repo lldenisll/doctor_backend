4be900b1c646711aa21eb95c5eadba260cf2be57
Denis <namorado@TFGcos-MacBook-Pro.local>
2021-04-04 18:02:48 -0300

    text field blank true

--- paciente/models.py
+++ paciente/models.py
@@ -7,7 +7,7 @@ class Paciente(models.Model):
     numero_acesso              = models.TextField(blank = True)
     data_estudo                = models.TextField(blank = True)
     espacamento_slice          = models.TextField(blank = True)
-    imagem                     = models.TextField()
+    imagem                     = models.TextField(blank = True)
     diagnostico                = models.CharField(blank = True, max_length=11,choices=[('pneumonia','Pneumonia'),('tuberculose','Tuberculose'),('normal','Normal')])
     grau_de_severidade         = models.CharField(blank = True,max_length=11,choices=[('grave','Grave'),('moderado','Moderado'),('leve','Leve'),('ausente','Ausente')])
     nivel_de_normalidade       = models.CharField(blank = True,max_length = 10, choices=[('tipico','Típico'),('atipico','Atípico')])
